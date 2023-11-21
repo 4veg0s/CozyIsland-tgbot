28c550ac7ae5861fd8a33d8b545c302b5eac38e4
Andrey Tkachev <tkachyov03@inbox.ru>
2023-11-21 21:10:16 +0300

    First commit

---
+++ src/main/java/com/cozyisland/CozyIslandtgbot/CozyIslandTgbotApplication.java
@@ -0,0 +1,13 @@
+package com.cozyisland.CozyIslandtgbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozyIslandTgbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozyIslandTgbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cozyisland/CozyIslandtgbot/CozyIslandTgbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozyisland.CozyIslandtgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozyIslandTgbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
